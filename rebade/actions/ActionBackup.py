53bc7cd66c1a8098e9f72b42c1d6f613ab4cbbc5
Johannes Bauer <joe@johannes-bauer.com>
2024-10-18 07:45:26 +0200

    Retry manual backup until successful
    When we have connection interruptions for large first-time backups, we
    want to try until we have a valid snapshot.

--- rebade/actions/ActionBackup.py
+++ rebade/actions/ActionBackup.py
@@ -19,6 +19,7 @@
 #
 #	Johannes Bauer <JohannesBauer@gmx.de>
 
+import time
 from rebade.MultiCommand import LoggingAction
 from rebade.Configuration import Configuration
 from rebade.BackupEngine import BackupEngine
@@ -28,9 +29,19 @@ class ActionBackup(LoggingAction):
 	def run(self):
 		self._config = Configuration.parse_json_file(self._args.config_file)
 		backup_engine = BackupEngine(self._args.restic_binary)
+		attempt_count = 0
 		with inhibit_suspend():
-			all_successful = True
-			for plan in self._config.get_plans_by_name(self._args.plan_name):
-				if not backup_engine.execute_backup(plan):
-					all_successful = False
+			while True:
+				attempt_count += 1
+				all_successful = True
+				for plan in self._config.get_plans_by_name(self._args.plan_name):
+					if not backup_engine.execute_backup(plan):
+						all_successful = False
+
+				if all_successful or self._args.oneshot:
+					break
+				else:
+					wait_time_secs = 60
+					print(f"Attempt #{attempt_count} was unsuccessful; retrying in {wait_time_secs} seconds...")
+					time.sleep(wait_time_secs)
 		return 0 if all_successful else 1
